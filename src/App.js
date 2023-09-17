6e62716bc596f79aac12c5aaa8652a45ec46e0b1
AllenTriplet <atriplet7@gmail.com>
2023-09-17 16:08:05 -0400

    main

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Default Portfolio page
         </a>
       </header>
     </div>
